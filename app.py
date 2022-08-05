1cc58aab4e9931554fca0b6b56b0624938d18289
Aman Gupta <aman.g@inmobi.com>
2022-08-05 22:17:13 +0530

    setting up

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-import json
+# import json
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-openai.api_key = "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"
+# openai.api_key = "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"
 
 @app.route("/")
 def index():
     return "Hello World!"
 
-@app.route('getContent/<string:prompt>/', methods=['GET'])
+@app.route('/getContent/<string:prompt>/', methods=['GET'])
 def get_content(prompt):
-    record = json.loads(request.data)
+    record = prompt
+    return record

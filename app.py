eb049eb7bc4a8e38a8ad0e4cef76ab2298a5d792
Aman Gupta <aman.g@inmobi.com>
2022-08-05 23:33:37 +0530

    added env

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+import os
 import openai
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-openai.api_key = "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"
+openai.api_key = os.environ['OPEN_AI_KEY']
 
 @app.route("/")
 def index():
@@ -11,14 +12,16 @@ def index():
 
 @app.route('/getContent/<string:prompt>/', methods=['GET'])
 def get_content(prompt):
-#     response = openai.Completion.create(
-#         model="text-davinci-002",
-#         prompt=prompt,
-#         temperature=0.5,
-#         max_tokens=60,
-#         top_p=1.0,
-#         frequency_penalty=0.0,
-#         presence_penalty=0.0
-#       )
-#     return jsonify({"status" : "ok", "result" : response.choices[0].text })
-    return prompt
+    try:
+        response = openai.Completion.create(
+                model="text-davinci-002",
+                prompt=prompt,
+                temperature=0.5,
+                max_tokens=60,
+                top_p=1.0,
+                frequency_penalty=0.0,
+                presence_penalty=0.0
+              )
+    except:
+        print("An exception occurred")
+    return jsonify({"status" : "ok", "result" : response.choices[0].text })

a0a23bccea8161c3c90f2df5f35b69ca0e6acefc
Aman Gupta <aman.g@inmobi.com>
2022-08-05 22:28:37 +0530

    Added open ai func

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-# import json
+import openai
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# openai.api_key = "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"
+openai.api_key = "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"
 
 @app.route("/")
 def index():
@@ -11,5 +11,13 @@ def index():
 
 @app.route('/getContent/<string:prompt>/', methods=['GET'])
 def get_content(prompt):
-    record = prompt
-    return record
+    response = openai.Completion.create(
+        model="text-davinci-002",
+        prompt=prompt,
+        temperature=0.5,
+        max_tokens=60,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+      )
+    return jsonify({"status" : "ok", "result" : response.choices[0].text })
